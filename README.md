6d8d8cf393f754923d4995da46979e015d3fb14f
sechang <ksc9590@gmail.com>
2020-12-27 22:28:00 +0900

    #6 Rooms Model Admin Panel

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 201221 first
 201222 21:38 ManyToMany ForeignKey
+201225 22:57 Review, Reservation, List, Conversation
+201227 22:26 Rooms Model Admin Panel
--- rooms/admin.py
+++ rooms/admin.py
@@ -13,7 +13,61 @@ class ItemAdmin(admin.ModelAdmin):
 class RoomAdmin(admin.ModelAdmin):
     """ Room Admin Definition """
 
-    pass
+    fieldsets = (
+        (
+            "Basic Info",
+            {"fields": ("name", "description", "country", "address", "price")},
+        ),
+        ("Times", {"fields": ("check_in", "check_out", "instant_book")}),
+        ("Spaces", {"fields": ("guests", "beds", "bedrooms", "baths")}),
+        (
+            "More About the Space",
+            {
+                "classes": ("collapse",),
+                "fields": ("amenities", "facilities", "house_rules"),
+            },
+        ),
+        ("Last Details", {"fields": ("host",)}),
+    )
+
+    list_display = (
+        "name",
+        "country",
+        "city",
+        "price",
+        "guests",
+        "beds",
+        "bedrooms",
+        "baths",
+        "check_in",
+        "check_out",
+        "instant_book",
+        "count_amenities",
+    )
+
+    list_filter = (
+        "instant_book",
+        "host__superhost",
+        "host__gender",
+        "room_type",
+        "amenities",
+        "facilities",
+        "house_rules",
+        "city",
+        "country",
+    )
+
+    search_fields = ["=city", "host__username"]  # forign key에 접근하는법
+
+    filter_horizontal = (
+        "amenities",
+        "facilities",
+        "house_rules",
+    )
+
+    def count_amenities(self, obj):
+        print(obj.amenities.all())
+        return "메롱"
 
 
 @admin.register(models.Photo)
